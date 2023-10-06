4afdadd68d9c18ce202c6769d91c68243226b228
Ryan McGee <ramtech51@comcast.net>
2023-10-05 20:44:10 -0400

    Initial project commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       ryan                                                      */
+/*    Created:      10/5/2023, 8:40:34 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+
+    Brain.Screen.printAt( 10, 50, "Hello V5" );
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
